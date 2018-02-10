bac66490835c3649a1eefc4cf9e84095063974bd
Matt Rosenquist <mrosenquist@andigitial.com>
2018-02-10 18:11:38 +0000

    refactor tests to support integration tests

--- tests/unit/processor/index.spec.js
+++ tests/unit/processor/index.spec.js
@@ -8,5 +8,30 @@ describe('processor', () => {
     test('points should be 0 with empty array', () => {
       expect(p.getPoints([])).toEqual(0);
     });
+
+    test('points should be 0 with empty no positive results', () => {
+      expect(p.getPoints([
+        { result: false, points: 0 },
+      ])).toEqual(0);
+    });
+
+    test('points should be 0 with empty no positive results', () => {
+      expect(p.getPoints([
+        { result: true },
+      ])).toEqual(0);
+    });
+
+    test('points should be 20', () => {
+      expect(p.getPoints([
+        { result: true, points: 10 },
+        { result: true, points: 10 },
+      ])).toEqual(20);
+    });
   });
+
+  // describe('#handleHeaders', () => {
+  //   test('points should be 0 with empty array', () => {
+  //     expect(p.handleHeaders([])).toEqual(0);
+  //   });
+  // });
 });
